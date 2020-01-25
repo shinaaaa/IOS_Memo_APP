d698ff938d193bb20cf963c85f48fe7e75d9de28
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-01-25 22:44:04 +0900

    icon 추가

---
+++ ExShinMemo/ExShinMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExShinMemo
+//
+//  Created by shin kim on 2020/01/25.
+//  Copyright © 2020 shin kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
