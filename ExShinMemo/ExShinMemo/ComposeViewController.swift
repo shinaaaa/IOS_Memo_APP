315e1922b862d13edd0d56924a7ffd8c18e0df29
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-01-25 23:35:55 +0900

    새 메모 취소 버튼

--- ExShinMemo/ExShinMemo/ComposeViewController.swift
+++ ExShinMemo/ExShinMemo/ComposeViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
