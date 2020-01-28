6549bec8e95a6f2bbca25647e0d02a75947cf17c
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-01-28 16:15:01 +0900

    ipad 모드 추가

--- ExShinMemo/ExShinMemo/DetailViewController.swift
+++ ExShinMemo/ExShinMemo/DetailViewController.swift
@@ -23,12 +23,16 @@ class DetailViewController: UIViewController {
     }()
     
     
-    @IBAction func share(_ sender: Any) {
+    @IBAction func share(_ sender: UIBarButtonItem) {
         guard let memo = memo?.content else {
             return 
         }
         
        let vc = UIActivityViewController(activityItems: [memo], applicationActivities: nil)
+        
+        if let pc = vc.popoverPresentationController {
+            pc.barButtonItem = sender
+        }
         
         present(vc, animated: true, completion: nil)
     }
